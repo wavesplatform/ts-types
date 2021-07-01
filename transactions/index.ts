c5be7774f18f9d2083b495e458a7f37ccf945582
enichegovskiy <eugene.nichegovskiy@gmail.com>
2021-07-01 12:48:17 +0300

    fix sponsorship tx fields

--- transactions/index.ts
+++ transactions/index.ts
@@ -163,7 +163,7 @@ export type SetScriptTransactionFields<LONG = Long> = {
 
 export type SponsorshipTransactionFields<LONG = Long> = {
     assetId: string;
-    minSponsoredAssetFee: LONG;
+    minSponsoredAssetFee: LONG | null;
 }
 
 export type SetAssetScriptTransactionFields<LONG = Long> = {
@@ -183,7 +183,7 @@ export type UpdateAssetInfoTransactionFields<LONG = Long> = {
     name: string;
     description: string;
     feeAssetId: string | null;
-}
+};
 
 export interface IUpdateAssetInfoTransaction<LONG = Long> extends BaseTransaction<LONG, typeof TRANSACTION_TYPE.UPDATE_ASSET_INFO>,
     UpdateAssetInfoTransactionFields<LONG> {
